29d772236b97d960f32aaa6dbeac710ddafe6497
Micky <sainimicky1807@gmail.com>
2023-02-03 11:25:04 +0530

    nextauth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,16 +4,21 @@ import { AiOutlineShopping } from 'react-icons/ai'
 
 import { Cart } from './';
 import { useStateContext } from 'context/StateContext';
-
+import { useSession, signIn, signOut } from "next-auth/react"
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
-
+  const { data: session } = useSession()
+  
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Book Shop</Link>
       </p>
-
+      <div onClick={!session ? signIn : signOut} className="hover-underline-animation">
+        <p className='sign'>
+          {session ? `Hello, ${session.user.email}` : "Sign In"}
+        </p>
+      </div>
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,16 @@ import '@/styles/globals.css'
 import { Layout } from 'components';
 import { StateContext } from 'context/StateContext';
 import { Toaster } from 'react-hot-toast';
-export default function App({ Component, pageProps }) {
+import {SessionProvider } from 'next-auth/react';
+export default function App({ Component, pageProps:{session, ...pageProps} ,}) {
   return (
+    <SessionProvider session={session}>
     <StateContext>
       <Layout>
       <Toaster/>
         <Component {...pageProps} />
       </Layout>
     </StateContext>
+    </SessionProvider>
   )
 }
