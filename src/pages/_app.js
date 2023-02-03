662c9678eacde57257dfaf744780589346ff465c
Micky <sainimicky1807@gmail.com>
2023-02-03 17:47:21 +0530

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import { Layout } from 'components';
 import { StateContext } from 'context/StateContext';
 import { Toaster } from 'react-hot-toast';
 import {SessionProvider } from 'next-auth/react';
-export default function App({ Component, pageProps}) {
+export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
     <StateContext>
