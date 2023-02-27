b4da5f6d657f5f2d0e0ea23a104d9b37f10a0b32
Micky <sainimicky1807@gmail.com>
2023-02-27 11:19:17 +0530

    hey

--- components/Product.jsx
+++ components/Product.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import { urlFor } from 'lib/client'
 
-const Product = ({ product: { image, name, slug, price } }) => {
+const Product = ({ product: { image, name, slug, price, category } }) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
@@ -15,6 +15,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
+          <p className="product-category">{category}</p>
           <p className="product-price">₹{price}</p>
         </div>
       </Link>
